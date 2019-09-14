9a4f472633bba36ce349853fffd87ab4f801e600
Juan <j.juancamilojurado@gmail.com>
2019-09-13 23:23:38 -0500

    Initial commit

---
+++ Taller4.2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Taller4._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnumeros = new System.Windows.Forms.TextBox();
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese cuantos números negativos\r\ndesea ver:";
+            // 
+            // txtnumeros
+            // 
+            this.txtnumeros.Location = new System.Drawing.Point(103, 70);
+            this.txtnumeros.Multiline = true;
+            this.txtnumeros.Name = "txtnumeros";
+            this.txtnumeros.Size = new System.Drawing.Size(100, 17);
+            this.txtnumeros.TabIndex = 1;
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmostrar.Location = new System.Drawing.Point(216, 149);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnmostrar.TabIndex = 2;
+            this.btnmostrar.Text = "Mostrar.";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.Btnmostrar_Click);
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(12, 124);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(191, 186);
+            this.lista.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Números pares.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 334);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnmostrar);
+            this.Controls.Add(this.txtnumeros);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnumeros;
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Taller4.2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller4._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnmostrar_Click(object sender, EventArgs e)
+        {
+            int numeros = int.Parse(txtnumeros.Text);
+           // int impar = 0;
+           
+            lista.Items.Clear();
+
+            for (int i =0; i <= numeros ; i++)
+            {
+
+                if (i % 2  == 0)
+                {
+                    lista.Items.Add("- " + i);
+                   
+                }
+
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
